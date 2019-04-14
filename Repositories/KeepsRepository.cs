91b5e8336c9bb43288f61f85b76db61454728b26
Luke King <lkingfrm@gmail.com>
2019-04-13 19:51:56 -0600

    tested and updated the keep routes

--- Repositories/KeepsRepository.cs
+++ Repositories/KeepsRepository.cs
@@ -32,13 +32,13 @@ namespace keepr.Repositories {
     }
 
     public Keep NewKeep (Keep newKeep) {
-      var madeKeep = _db.Execute ("INSERT INTO keeps (name,description,userId,img,isPravate values(@Name, @Description, @UserId, @IsPrivate))", newKeep);
+      var madeKeep = _db.Execute ("INSERT INTO keeps (name,description,userId,img,isPrivate) values(@Name, @Description, @UserId, @Img, @IsPrivate)", newKeep);
 
       return madeKeep > 0 ? newKeep : null;
     }
 
     public Keep EditKeep (Keep keep) {
-      var editKeep = _db.Execute ("UPDATE keeps SET id = @Id name = @Name,description = @Description, userId = @UserId, img = @Img, isPrivate = @IsPrivate, views = @Views, shares = @Shares, keeps = @Keeps WHERE id = @Id", keep);
+      var editKeep = _db.Execute ("UPDATE keeps SET id = @Id, name = @Name,description = @Description, userId = @UserId, img = @Img, isPrivate = @IsPrivate, views = @Views, shares = @Shares, keeps = @Keeps WHERE id = @Id", keep);
 
       return editKeep > 0 ? keep : null;
     }
